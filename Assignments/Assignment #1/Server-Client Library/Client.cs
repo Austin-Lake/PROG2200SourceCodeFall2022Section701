0cb3be73707e7732aca7a1e7db6775cd1f8a4606
Austin <90702422+Austin-Lake@users.noreply.github.com>
2022-10-24 22:09:53 -0300

    Assignment #1 Client Implemented

---
+++ Assignments/Assignment #1/Server-Client Library/Client.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server_Client_Library
+{
+    public class Client
+    {
+        public Client(String server, Int32 port)
+        {
+            try
+            {
+             
+                TcpClient client = new TcpClient(server, port);
+
+                NetworkStream stream = client.GetStream();
+                Console.WriteLine("Connected to server!");
+
+                string clientInput = "";
+                Console.WriteLine("Type exit to exit program or press the escape key");
+                Console.WriteLine("Write a message");
+                while (true) 
+                {
+                    Console.Write(">>");
+                    clientInput = Console.ReadLine();
+                    bool isKey = false;
+                    if (Console.KeyAvailable)
+                    {
+                        isKey = Console.ReadKey().Equals(ConsoleKey.Escape);
+                    }
+                    if (!clientInput.Equals("Exit".ToLower()))
+                    {
+                        Byte[] data = System.Text.Encoding.ASCII.GetBytes(clientInput);
+                        stream.Write(data, 0, data.Length);
+                        data = new Byte[256];
+                        String responseData = String.Empty;
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                        Console.WriteLine("{0}", responseData);
+                    }
+                    else 
+                    {
+                        break;
+                    }
+                }
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+        }
+    }
+}
---
+++ Assignments/Assignment #1/Synchronous Chat Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using Server_Client_Library;
+
+
+    namespace Synchronous_Chat_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 port = 13000;
+            string address = "127.0.0.1";
+
+            if (args.Length == 0 || args[0].ToLower() == "-client")
+            {
+                Client client = new Client(address, port);
+            }
+            else if (args[0].ToLower() == "-server") 
+            {
+                Server server = new Server(address, port);
+            }
+        }
+    }
+}
